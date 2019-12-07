1af56f4ed3afae856b63ecb511d786761f6f04bc
pokkst <pokkst@protonmail.com>
2019-12-07 17:54:51 -0600

    Prep BIP70 tests class

---
+++ app/src/androidTest/java/com/bitcoin/merchant/app/BIP70InvoiceTests.java
@@ -0,0 +1,34 @@
+package com.bitcoin.merchant.app;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class BIP70InvoiceTests {
+    @Test
+    public void createInvoice() {
+        //TODO
+    }
+
+    @Test
+    public void listenForInvoice() {
+        //TODO
+    }
+
+    @Test
+    public void listenThenStop() {
+        //TODO
+    }
+
+    @Test
+    public void listenForPaidStatus() {
+        //TODO
+    }
+}
