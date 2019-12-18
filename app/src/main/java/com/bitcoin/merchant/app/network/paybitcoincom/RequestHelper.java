a7be78c63e826939466e2fbb8188af7d9246c8ad
pokkst <pokkst@protonmail.com>
2019-12-17 20:35:14 -0600

    Setup RequestHelper methods for registering merchant and using pairing codes

--- app/src/main/java/com/bitcoin/merchant/app/network/paybitcoincom/RequestHelper.java
+++ app/src/main/java/com/bitcoin/merchant/app/network/paybitcoincom/RequestHelper.java
@@ -8,7 +8,16 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class RequestHelper {
-    public String post(String endpoint, String json) throws IOException {
+    //TODO use the actual endpoints rather than these placeholders since i can't remember the real ones right now
+    public String usePairingCode(String json) throws IOException {
+        return this.post("use-pairing-code", json);
+    }
+
+    public String registerMerchant(String json) throws IOException {
+        return this.post("register-merchant", json);
+    }
+
+    private String post(String endpoint, String json) throws IOException {
         URL url = new URL("https://api.pay.bitcoin.com/" + endpoint);
         HttpURLConnection con = (HttpURLConnection)url.openConnection();
         con.setDoOutput(true);
