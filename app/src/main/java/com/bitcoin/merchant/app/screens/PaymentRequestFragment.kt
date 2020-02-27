71837c97ed3a8fe580521683bd1699c3c83e7278
pokkst <pokkst@protonmail.com>
2020-02-27 08:31:21 -0600

    Re-add share intent method in PaymentRequestFragment.kt

--- app/src/main/java/com/bitcoin/merchant/app/screens/PaymentRequestFragment.kt
+++ app/src/main/java/com/bitcoin/merchant/app/screens/PaymentRequestFragment.kt
@@ -370,6 +370,17 @@ class PaymentRequestFragment : ToolbarAwareFragment() {
         }
     }
 
+    private fun startShareIntent(paymentUrl: String) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "Please pay your invoice here: $paymentUrl")
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+
     override val isBackAllowed: Boolean
         get() {
             return backButtonAllowed
